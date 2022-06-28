b797cc794e25b429f26d83df07f67d4a80e2640c
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-06-28 10:27:26 +0530

    Add files via upload

---
+++ KthLargest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class KthLargest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter size:");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter array:");
+		for(int i=0;i<n;i++)
+			arr[i]=sc.nextInt();
+		System.out.println("Enter value of k:");
+		int k=sc.nextInt();
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]<arr[j])
+				{
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+			if(i==k-1)
+				System.out.println(k+"th largest element is "+arr[i]);
+		}
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+
+}
