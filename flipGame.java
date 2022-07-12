e6f352e7a23c50c1808836bac057fab5a0aea68f
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-07-12 17:12:01 +0530

    Add files via upload

---
+++ flipGame.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class flipGame {
+	public static List<String> generatePossibleNextMoves(String s) {
+	    List<String> result = new ArrayList<String>();
+	 
+	    if(s==null)
+	        return result;
+	 
+	    char[] arr = s.toCharArray();
+	    for(int i=0; i<arr.length-1; i++){
+	        if(arr[i]==arr[i+1] && arr[i]=='+'){
+	            arr[i]='-';
+	            arr[i+1]='-';
+	            result.add(new String(arr));
+	            arr[i]='+';
+	            arr[i+1]='+';
+	        }
+	    }
+	 
+	    return result;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="++++";
+		System.out.println(generatePossibleNextMoves(s));
+	}
+
+}
