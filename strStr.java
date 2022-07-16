34cae9ca0b27ad0c44e9885d919ca7891127bcaf
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-07-16 17:57:56 +0530

    Add files via upload

--- strStr.java
+++ strStr.java
@@ -2,25 +2,29 @@ import java.util.Scanner;
 
 public class strStr {
 	
-	int str_Str(String haystack,String needle) {
-		if(needle.isEmpty())
-			return 0;
-			
-		if(haystack.length()<needle.length())
-			return -1;
-		
-		for(int i=0;i<haystack.length()-needle.length();i++)
-		{
-			int j;
-			for(j=0;j<needle.length();j++) {
-				if(haystack.charAt(i+j) != needle.charAt(j))
-					break;
-			}
-			if(j==needle.length())
-				return i;
-		}
-		return -1;
-	}
+	static int str_Str(String haystack,String needle) {
+		 if(needle.isEmpty()){
+	            return 0;
+	        }
+	        if(haystack.length()<needle.length()){
+	            return -1;
+	        }
+	        
+	        for(int i=0;i<haystack.length()-needle.length()+1;i++){
+	            if(haystack.charAt(i)==needle.charAt(0)){
+	                for(int j=0;j<needle.length();j++){
+	                    if(haystack.charAt(i+j)!=needle.charAt(j)){
+	                        break;
+	                    }
+	                    if(j==needle.length()-1){
+	                        return i;
+	                    }
+	                }
+	                
+	            }
+	        }
+	        return -1;
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -31,9 +35,7 @@ public class strStr {
 		System.out.println("Enter needle");
 		String needle=sc.nextLine();
 		
-		strStr obj=new strStr();
-		int result=obj.str_Str(haystack, needle);
-		System.out.println(result);
+		System.out.println(str_Str(haystack, needle));
 		
 	}
 	
