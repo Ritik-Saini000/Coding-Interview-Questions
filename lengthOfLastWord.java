fbdeadb3d12ce2751f9b6864d058a067b8eb119d
Ritik Saini <86900692+Ritik-Saini000@users.noreply.github.com>
2022-07-16 19:34:01 +0530

    Add files via upload

--- lengthOfLastWord.java
+++ lengthOfLastWord.java
@@ -4,14 +4,19 @@ public class lengthOfLastWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String str="Hello World";
-		int n=str.length(),result=0;
-		while(n>0) {
-			if(str.charAt(--n) != ' ')
+		String str="   fly me   to   the moon  ";
+		int result=0;
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			if(str.charAt(i)!=' ')
+			{
 				result++;
-			else if(result>0)
-				System.out.println(result);
+				continue;
+			}
+			if(result>0)
+				break;
 		}
+		System.out.println(result);
 	}
 
 }
